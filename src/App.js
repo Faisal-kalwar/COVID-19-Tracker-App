7bd45f943bbce8a210e650e44f8816524dabe794
Faisal-kalwar <faisalkalwar48@gmail.com>
2022-06-22 13:33:17 +0500

    InfoPanel adddes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './Components/NavBar'
+import InfoBar from './Components/InfoPanel'
 
 function App() {
   return (
     <div>
       <NavBar />
+      < InfoBar />
     </div>
   );
 }
